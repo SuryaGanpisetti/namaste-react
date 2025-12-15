310dcfb712bcf43b10b967b720790cae14cfc1a4
SuryaGanpisetti <suryateja229@gmail.com>
2025-12-15 22:53:12 +0530

    episode-01

---
+++ app.js
@@ -0,0 +1,13 @@
+const heading = React.createElement("h1", {id: "heading", xyz: "abc"}, "HelloWorld From React!");
+console.log(heading);
+const parent = React.createElement("div", {id: "parent"},[
+               React.createElement("div", {id: "child"},[ 
+               React.createElement("h1",  {id: "h1"}, "I am an H1 tag"), 
+               React.createElement("h2",  {id: "h2"}, "I am an H2 tag")]),
+               React.createElement("div", {id: "child2"},[ 
+               React.createElement("h1",  {id: "h1"}, "I am an H1 tag"),
+               React.createElement("h2",  {id: "h2"}, "I am an H2 tag")]
+               )]);
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
